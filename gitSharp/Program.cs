93b21e139a26ea8143612efcecc6ca9960c08cc8
Timiks1 <prozor2@ukr.net>
2022-11-05 19:25:28 +0200

    first commit

--- gitSharp/Program.cs
+++ gitSharp/Program.cs
@@ -1,4 +1,21 @@
-﻿Console.WriteLine("enter speed");
+﻿Console.WriteLine("введите ваш тип покрытия:\n1 - сухой асфальт\n2 - мокрый асфльт\n3 - асфальт покрытый снегом\n4 - асфальт покрытый льдом");
+int type = int.Parse(Console.ReadLine());
+Console.WriteLine("введите вашу скорость в км/ч");
 int speed = int.Parse(Console.ReadLine());
 
-Console.WriteLine((speed/10)*(speed/10));
+double trueType = 0.7;
+if(type == 1)
+{
+    trueType = 0.7;
+}
+else if(type == 2)
+{
+    trueType = 0.4;
+}
+else if (type == 3)
+{
+    trueType = 0.02;
+}
+
+double pass = (speed * speed) / (254 * trueType);
+Console.WriteLine("ваш тормозной путь = "+pass + " метров");
